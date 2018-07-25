34b5c8dc10c95a1e80a70161b51fdc3d22ba8dd3
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2018-07-25 00:55:15 -0300

    typo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::UsersController < ApplicationController
   def show
     begin
       @user = User.find(params[:id])
+      
       respond_with @user
     rescue
       head 404
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,34 +1,34 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "User API", type: :request do
+RSpec.describe 'User API', type: :request do
   let!(:user)   { create :user }
   let(:user_id) { user.id }
 
-  before { host! "api.spec.test" }
+  before { host! 'api.spec.test' }
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      headers = { "Accept" => "application/vnd.taskmanager.v1" }
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
 
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
-    context "when users exists" do
-      it "returns the user" do
+    context 'when users exists' do
+      it 'returns the user' do
         user_response = JSON.parse(response.body)
 
-        expect(user_response["id"]).to eq user_id
+        expect(user_response['id']).to eq user_id
       end
 
-      it "returns status 200" do
+      it 'returns status 200' do
         expect(response).to have_http_status 200
       end
     end
 
-    context "when users does not exists" do
+    context 'when users does not exists' do
       let(:user_id) { 0 }
 
-      it "returns status 200" do
+      it 'returns status 200' do
         expect(response).to have_http_status 404
       end
     end
