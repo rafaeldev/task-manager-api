f2005c4961994d6e0f75fec5af6cd0cbd7f08c3c
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2018-07-29 12:53:17 -0300

    feat: create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,28 @@ class Api::V1::UsersController < ApplicationController
   def show
     begin
       @user = User.find(params[:id])
-      
+
       respond_with @user
     rescue
       head 404
     end
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email, :name, :password, :password_confirmation
+    )
   end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -33,4 +33,40 @@ RSpec.describe 'User API', type: :request do
       end
     end
   end
+
+  describe 'POST /users' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+
+      post '/users', params: { user: user_params }, headers: headers
+    end
+
+    context 'when the request parameters are valid' do
+      let(:user_params) { attributes_for :user }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status 201
+      end
+
+      it 'returns json data for the created user' do
+        user_response = JSON.parse(response.body)
+
+        expect(user_response['email']).to eq user_params[:email]
+      end
+    end
+
+    context 'when the request parameters are invalid' do
+      let(:user_params) { attributes_for :user, email: 'invalid' }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status 422
+      end
+
+      it 'returns json data for the errors' do
+        user_response = JSON.parse(response.body)
+
+        expect(user_response).to have_key 'errors'
+      end
+    end
+  end
 end
