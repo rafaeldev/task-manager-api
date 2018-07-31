53600f7b5873b13d466ecd761cc5fe4be86d1187
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2018-07-30 23:27:20 -0300

    feat: destroy action to session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -13,6 +13,15 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find_by(auth_token: params[:id])
+
+    user.generate_authentication_token!
+    user.save
+
+    head 204
+  end
+
   def sessions_params
     params.require(:session).permit(:email, :password)
   end
